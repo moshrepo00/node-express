1864c4a82d6b55770e28d544e58578483f46bfeb
mo <moshpit2241@gmail.com>
2019-03-14 02:14:41 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+// initialize our express app
+const product = require('./routes/product.route'); // Imports routes for the products
+
+const app = express();
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+
+// Set up mongoose connection
+let dev_db_url = 'mongodb+srv://mos:test@cluster0-cgltt.mongodb.net/test?retryWrites=true';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/products', product);
+
+let port = 8000;
+
+app.listen(port, () => {
+	console.log('Server is up and running on port number ' + port);
+});
